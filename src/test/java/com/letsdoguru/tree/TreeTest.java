24c0521a99cf2dc348256d3aff36169995d412ee
Gaurav Handa <gauravhanda21@yahoo.com>
2020-12-11 15:55:04 -0600

    Added unit test cases

--- src/test/java/com/letsdoguru/tree/TreeTest.java
+++ src/test/java/com/letsdoguru/tree/TreeTest.java
@@ -1,6 +1,7 @@
 package com.letsdoguru.tree;
 
 import com.letsdoguru.model.Employee;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,5 +23,26 @@ public class TreeTest {
         BinaryTree<Employee> employeeBinaryTree = new BinaryTree<>((emp1, emp2) -> emp1.getAge() <= emp2.getAge() ? -1 : 1);
         employees.stream().forEach(employeeBinaryTree::addNode);
         employeeBinaryTree.print(BinaryTree.TraversalType.IN_ORDER);
+    }
+
+    @Test
+    public void findEmployeeByName() {
+        BinaryTree<Employee> employeeBinaryTree = new BinaryTree<>((emp1, emp2) -> emp1.getName().compareTo(emp2.getName()));
+        employees.stream().forEach(employeeBinaryTree::addNode);
+        Assert.assertTrue(employeeBinaryTree.isPresent(new Employee("Gaurav", (byte) 41)));
+    }
+
+    @Test
+    public void findEmployeeByNameButDifferentAge() {
+        BinaryTree<Employee> employeeBinaryTree = new BinaryTree<>((emp1, emp2) -> emp1.getName().compareTo(emp2.getName()));
+        employees.stream().forEach(employeeBinaryTree::addNode);
+        Assert.assertTrue(employeeBinaryTree.isPresent(new Employee("Gaurav", (byte) 41)));
+    }
+
+    @Test
+    public void findNonExistingEmployee() {
+        BinaryTree<Employee> employeeBinaryTree = new BinaryTree<>((emp1, emp2) -> emp1.getName().compareTo(emp2.getName()));
+        employees.stream().forEach(employeeBinaryTree::addNode);
+        Assert.assertFalse(employeeBinaryTree.isPresent(new Employee("Gaurav1", (byte) 41)));
     }
 }
